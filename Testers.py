17da31aafce3caca450a81a1746a05710ab66ff1
DanielKesel <dkesel@frba.utn.edu.ar>
2023-05-07 20:05:56 -0300

    avance

--- Testers.py
+++ Testers.py
@@ -11,6 +11,7 @@ TOCIO = [] #Tiempo Ocioso
 TTESTING = [] #Tiempo de duracion del Testing que varia segun prioridad.
 TIEMPO = 0
 SUMATORIA_TLL = 0
+SUMATORIA_TLL = 0
 IA = 0
 TIEMPO_FINAL = 0
 HV = math.inf
@@ -96,48 +97,56 @@ TIEMPO_FINAL = int(input("Ingrese el número de días: "))
 cantidad_de_tareas_asignables = int(input("Ingrese la cantidad maxima de tareas que se pueden asignar: "))
 cantidad_de_testers = int(input("Ingresá la cantidad de testers que quieres contratar:\n -> "))
 print("La cantidad de testers que has decidido contratar es %s" %cantidad_de_testers)
+
 TPS = [HV] * cantidad_de_testers
-print(TPS)
+SUMATORIA_TIEMPO_OSCIOSO = [0] * cantidad_de_testers
+INICIO_TIEMPO_OSCIOSO = [0] * cantidad_de_testers
 vector_puestos_de_atencion = [0] * cantidad_de_testers
 cantidad_de_testers_disponibles = cantidad_de_testers
-
-print("tamaño de lista ", TPS)
 contador_tareas = 0
 contador_dias_sin_tareas = 0
 espera_tarea = bool
+
 while TIEMPO < TIEMPO_FINAL :
-    print("entro a tiempo menos tiempo final")
-    print(TPS[buscarMenorTPS()])
+    print("-> Entramos a la condicion 'TIEMPO < TIEMPO_FINAL'")
+    
     if TPLL <= TPS[buscarMenorTPS()]:
-        
+        print("-> Entramos a la condicion 'TPLL <= TPS[buscarMenorTPS()]'")
         TIEMPO = TPLL
         SUMATORIA_TLL += TPLL
-        print("entro a TPS MENOS TPLL ")
         while espera_tarea :
             R = random.random()
             if R >= 0.5 :  
-                
+                print("-> Entramos a la condicion 'llegaTarea()'")
                 contador_tareas += 1
                 tareaAceptada = llegadaTarea()
-                TPLL += TIEMPO + IA
                 espera_tarea = False
     
             else :
+                print("-> Entramos a la condicion 'No llega tarea e incrementa intervalo de arribo'")
                 IA += 1
 
         espera_tarea = True
         if tareaAceptada == True :
+            print("-> Entramos a la condicion 'tareaAceptada == True'")
+            TPLL += TIEMPO + IA
             contador_tareas_atendidas += 1
             prioridad_llegada = FDPprioridad(BP, MP, AP)
             if(BP + MP + AP) <= cantidad_de_testers_disponibles :
+                print("-> Entramos a la condicion 'if(BP + MP + AP) <= cantidad_de_testers_disponibles'")
                 if HV in TPS :     
+                    print("-> 'HV in TPS'")
                     posicion_disponible = buscarHV()
                     TPS[posicion_disponible] = TIEMPO + FDPresolucionTareas(prioridad_llegada)
-                    SUMATORIA_TIEMPO_OSCIOSO[posicion_disponible] += TIEMPO - INICIO_TIEMPO_OSCIOSO
+                    SUMATORIA_TIEMPO_OSCIOSO[posicion_disponible] += TIEMPO - INICIO_TIEMPO_OSCIOSO[posicion_disponible]
             
-            
-        print("Tarea que retorna a la cola")
-        
+        print("TPLL es %s y TPS es %s" %(TPLL, TPS))
+        print("Se vuelve a cumplir el ciclo hasta que deje de pasar TIEMPO < TIEMPO_FINAL")
+
+    #Tiempo Proxima Salidaelse : 
+    #Tiempo Proxima Salida    TIEMPO = TPS[buscarMenorTPS()]
+
+    TIEMPO +=1
 
 print("En un lapso de %s días llegaron %s tareas, se rechazaron %s y se atendieron %s" % (TIEMPO_FINAL, contador_tareas, arrepentidos ,contador_tareas_atendidas))
 
